bf07265f5fc882d540dc853fadb7aca444b34765
ikemura-kei <ikemurakei2001@gmail.com>
2026-05-19 22:32:51 +0200

    initial commit

---
+++ tests/check_dependencies.py
@@ -0,0 +1,10 @@
+import stable_worldmodel
+from rich import print
+import sys
+
+def main():
+    print("Python version:", sys.version)
+    print("Hello from cloudgripper-wm!")
+
+if __name__ == "__main__":
+    main()
