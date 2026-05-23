c4133268a63f80de1dd57caa0d573dfb5799dda2
ikemura-kei <ikemurakei2001@gmail.com>
2026-05-23 22:06:43 +0200

    update

--- scripts/train/lewm.py
+++ scripts/train/lewm.py
@@ -135,11 +135,12 @@ def run(cfg):
         train_set,
         **cfg.loader,
         generator=rnd_gen,
+        multiprocessing_context='fork',
     )
     val_cfg = {**cfg.loader}
     val_cfg['shuffle'] = False
     val_cfg['drop_last'] = False
-    val = torch.utils.data.DataLoader(val_set, **val_cfg)
+    val = torch.utils.data.DataLoader(val_set, **val_cfg, multiprocessing_context='fork')
 
     ##############################
     ##       model / optim      ##
